0728e726bf3ef8d8b5feffb59018cd0302197ef6
Rohit-kashyap1014 <40514216+Rohit-kashyap1014@users.noreply.github.com>
2018-11-16 21:10:38 +0530

    Add files via upload

---
+++ Number wiard UI/Assets/NumberWizard.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour {
+
+	// Use this for initialization
+	int max;
+	int min;
+	int guess;
+	
+	int maxguessallowed = 10;
+	 
+	public Text text;
+	 
+	void Start () {
+	
+	startgame();
+	
+	}
+	
+	void startgame() {
+	    max = 1000;
+	    min = 1;
+		nextguess();
+		
+	
+        max = max + 1;	
+	}
+	
+	// Update is called once per frame
+	
+	public void GuessLower() {
+		max = guess;
+		nextguess();
+	
+	}
+	
+	public void GuessHigher() {
+		min = guess;
+		nextguess();
+	
+	}
+	
+	void nextguess() {
+	
+		guess = Random.Range (min,max+1);
+		text.text = guess.ToString();
+		maxguessallowed = maxguessallowed - 1;
+		if(maxguessallowed<=0) {
+		Application.LoadLevel("win");
+		}
+	
+	}
+	
+}
---
+++ Number wiard UI/Assets/levelmanage.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class levelmanage : MonoBehaviour {
+
+	public void LoadLevel(string name) {
+	
+	Debug.Log ("level load requested for " +name);
+	Application.LoadLevel(name);
+	
+	}
+	
+	public void QuitRequest() {
+	 
+	Debug.Log ("I want to quit"); 
+	Application.Quit();
+	
+	}
+}
